0b2ad486988d436a5a8282772707e8f211906ba2
David <ermesdavidg.a@gmail.com>
2022-12-03 21:48:50 -0600

    subir a githut desde aqui

--- Pizeria/models.py
+++ Pizeria/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from  django.conf import settings
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Pizza(models.Model):
-    Nombre=models.CharField(max_length=20, blank=True, null=True)
-    Medida= models.CharField(max_length=20, blank=True, null=True)
-    Cantidad= models.IntegerField()
-    Costo=models.IntegerField()
+    Nombre=models.CharField(max_length=20, blank=False, null=False)
+    Medida= models.CharField(max_length=20, blank=False, null=False)
+    Cantidad= models.IntegerField(blank=False, null=False, validators=[MinValueValidator(0),MaxValueValidator(1000)])
+    Costo=models.DecimalField(max_digits=5, max_length=8, blank=False, null=False,decimal_places=2)
